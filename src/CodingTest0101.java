4385927308d9c47a16885c82f1eaa317072d9252
ParkBeomsoo <68089887+ParkBeomsoo@users.noreply.github.com>
2023-08-01 23:20:29 +0900

    feat: 자바프로젝트로 변경 및 코딩테스트 0101추가

---
+++ src/CodingTest0101.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CodingTest0101 {
+    public int solution(String str, char t){
+        int answer=0;
+        for(char x : str.toCharArray()){
+            if(Character.toUpperCase(x) == Character.toUpperCase(t))
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CodingTest0101 codingTest0101 = new CodingTest0101();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.println("adsf");
+        System.out.print(codingTest0101.solution(str, c));
+
+    }
+}
